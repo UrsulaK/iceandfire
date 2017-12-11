502a0a0fb3788435ecd70fb851d8fe50d9f56f85
Ursula Kiekheben <u.kiekheben@gmail.com>
2017-12-11 17:00:21 +0100

    frontend service

---
+++ iceandfire_frontend/src/main/java/iceandfire/IceandfireFrontendApplication.java
@@ -0,0 +1,12 @@
+package iceandfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceandfireFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceandfireFrontendApplication.class, args);
+	}
+}
