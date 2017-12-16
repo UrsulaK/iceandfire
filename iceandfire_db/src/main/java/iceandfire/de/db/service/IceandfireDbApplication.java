c2d893648782e9ec8147641bde07358d3e8b0fae
Ursula Kiekheben <u.kiekheben@gmail.com>
2017-12-16 09:28:48 +0100

    db service

---
+++ iceandfire_db/src/main/java/iceandfire/de/db/service/IceandfireDbApplication.java
@@ -0,0 +1,12 @@
+package iceandfire.de.db.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceandfireDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceandfireDbApplication.class, args);
+	}
+}
