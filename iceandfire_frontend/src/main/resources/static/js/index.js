659be77e1b2ee7356b276765d6e0cd7203faceed
Ursula Kiekheben <u.kiekheben@gmail.com>
2017-12-16 09:25:32 +0100

    frontend search

---
+++ iceandfire_frontend/src/main/resources/static/js/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	
+	var search = function(){
+		
+		$("#content").empty();
+		var baseUrl = "http://localhost:8080/iceandfire/houses/"
+		var region = $( "#region option:selected" ).text();
+		var paging = "/1/10";
+		var url = baseUrl.concat(region, paging);
+		
+		$.get(url, function(data) {
+			
+			for(var i=0; i<data.length; i++){
+				
+				var tr = $("<tr/>");
+				var td = $("<td/>");
+				td.append($("<div class='lead' />").text(data[i].name));
+				td.append($("<div />").text(data[i].coatOfArms));
+				td.append($("<div class='text-muted' />").text(data[i].words));
+				
+				var td2 = $("<td/>")
+				var swornMembers = data[i].swornMembers;
+				for(var j=0; j<swornMembers.length; j++){
+					td2.append($("<div class='text-muted' />").text(swornMembers[j]));
+				}
+				
+				tr.append(td);
+				tr.append(td2);
+				$("#content").append(tr);
+			}
+		});
+	};
+	$("#search").on("click", search);
+})
