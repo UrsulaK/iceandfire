c7ee2e7cbaa11c0995fa03088f8272ca9b2676f2
Ursula Kiekheben <u.kiekheben@gmail.com>
2017-12-24 18:19:58 +0100

    initial commit

---
+++ iceandfire_service/src/main/java/iceandfire/de/service/IceAndFireServiceApplication.java
@@ -0,0 +1,12 @@
+package iceandfire.de.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceAndFireServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceAndFireServiceApplication.class, args);
+	}
+}
