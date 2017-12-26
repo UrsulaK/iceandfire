ded2012b27dab8f164c809c2240bffa087c20127
Ursula Kiekheben <u.kiekheben@gmail.com>
2017-12-26 06:13:49 +0100

    search methods

--- iceandfire_db/src/main/java/iceandfire/de/db/service/repository/HouseRepository.java
+++ iceandfire_db/src/main/java/iceandfire/de/db/service/repository/HouseRepository.java
@@ -1,11 +1,17 @@
 package iceandfire.de.db.service.repository;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import iceandfire.de.db.service.model.House;
 
 public interface HouseRepository extends PagingAndSortingRepository<House, String>{
 
-	
+	Page<House> findByNameLike(@Param("name") String name, Pageable pageable);
+	Page<House> findByRegionLike(@Param("region") String region, Pageable pageable);
+	Page<House> findByNameLikeAndRegionLike(@Param("name") String name, @Param("region") String region, Pageable pageable );
+
 }
--- iceandfire_db/src/main/java/iceandfire/de/db/service/repository/SwornMemberRepository.java
+++ iceandfire_db/src/main/java/iceandfire/de/db/service/repository/SwornMemberRepository.java
@@ -4,15 +4,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import iceandfire.de.db.service.model.SwornMember;
 
 public interface SwornMemberRepository extends PagingAndSortingRepository<SwornMember, String>{
 	
-//	Page<SwornMember> findFirstnameLikeOrLastnameLikeOrNameLike(String firstname, String lastname, String name, Pageable pageable);
-//	@Query("{ 'candidate' : ?0 }")
-//	default Page<SwornMember> findByFirstnameOrLastnameOrName(String candidate, Pageable pageable) {
-//		return findFirstnameLikeOrLastnameLikeOrNameLike(candidate, candidate, candidate, pageable );
+	
+	Page<SwornMember> findByFirstnameLikeOrLastnameLikeOrNameLike(@Param("firstname") String firstname, @Param("lastname") String lastname, @Param("name") String name, Pageable pageable);
+	
+	Page<SwornMember> findByNameLike(@Param("name") String name, Pageable pageable);
+	
+	
+//	default Page<SwornMember> findByFirstnameLikeOrLastnameLikeOrNameLike(@Param("name")String name, Pageable pageable) {
+//		return findByFirstnameLikeOrLastnameLikeOrNameLike(name, name, name, pageable );
 //	}
 		
 }
