157b30a0a1313f9fd6129a682b6fb11a25ad7b4d
Ursula Kiekheben <u.kiekheben@gmail.com>
2018-01-14 13:12:31 +0100

    changed graphite host

--- iceandfire_db/src/main/java/iceandfire/de/db/service/config/GraphiteConfiguration.java
+++ iceandfire_db/src/main/java/iceandfire/de/db/service/config/GraphiteConfiguration.java
@@ -21,7 +21,7 @@ public class GraphiteConfiguration {
 
     @PostConstruct
     public void initialize() {
-        final Graphite graphite = new Graphite(new InetSocketAddress("localhost", 2003));
+        final Graphite graphite = new Graphite(new InetSocketAddress("0.0.0.0", 2003));
         final GraphiteReporter reporter = GraphiteReporter.forRegistry(registry)
                 .prefixedWith("iceandfire.db")
                 .convertRatesTo(TimeUnit.SECONDS)
